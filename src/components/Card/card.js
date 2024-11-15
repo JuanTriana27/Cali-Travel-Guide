b3f2be6c5799f5e57fd4d43ddbe4f18ca42d6e7b
Juan Esteban <trianajuan28@gmail.com>
2024-11-15 12:51:20 -0500

    Tercer Commit

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -4,8 +4,8 @@ import '../Card/card.css';
 
 // Importa cada imagen con un nombre específico
 import recorrido from '../../assets/imagenes/Recorrido.jpg';
-import cristoRey from '../../assets/imagenes/CristoRey.jpg';
-import fotografias from '../../assets/imagenes/Fotografias.jpg';
+import fotografias from '../../assets/imagenes/CristoRey.jpg';
+import cristoRey from '../../assets/imagenes/Fotografias.jpg';
 
 // Creamos un componente funcional Card que recibe dos propiedades: title y subtitle
 const Card = ({ title, subtitle }) => {
@@ -18,14 +18,19 @@ const Card = ({ title, subtitle }) => {
     imgSrc = fotografias;
   }
 
-  // Retornamos un div con la imagen, el título y el subtítulo
+  // Retornamos un div con la imagen, el título, el subtítulo y el overlay rojo
   return (
     <div className="card">
       <img src={imgSrc} alt={title} className="cardImage" />
+      
+      {/* Contenido de la tarjeta */}
       <div className="cardContent">
         <h2 className="cardTitle">{title}</h2>
         <p className="cardSubtitle">{subtitle}</p>
       </div>
+
+      {/* Overlay rojo que aparece desde abajo */}
+      <div className="cardOverlay"></div>
     </div>
   );
 };
