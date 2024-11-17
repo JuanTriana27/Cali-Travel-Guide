50a4eb1c5dc356c9e418df901b51faf14de804f3
Juan Esteban <trianajuan28@gmail.com>
2024-11-17 11:42:07 -0500

    Commit 11

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,11 +1,16 @@
-// src/components/Navbar/Navbar.js
-import React from 'react';
-import '../Navbar/navbar.css';
+import React from "react";
+import "./navbar.css";
 
 const Navbar = () => (
   <nav className="navbar">
     <h1>CALI TRAVEL GUIDE</h1>
-    <ul className="navbarMenu">
+
+    {/* Checkbox para el menú hamburguesa */}
+    <input type="checkbox" id="menu-toggle" />
+    <label htmlFor="menu-toggle" id="menu-icon">☰</label>
+
+    {/* Menú */}
+    <ul className="navbarMenu" id="menu-icon-acction">
       <li>INICIO</li>
       <li>LUGARES</li>
       <li>CONTACTENOS</li>
