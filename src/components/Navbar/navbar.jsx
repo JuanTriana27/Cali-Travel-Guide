c36b27743aad51349bc28ffc5b8db1c3e8220dee
Juan Esteban <trianajuan28@gmail.com>
2025-03-08 17:02:42 -0500

    Bugs y errores

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,24 +1,28 @@
-import React from "react"; // Importar la librería React
-import { Link } from 'react-router-dom'; // Link para navegación
-import "./navbar.css"; // Estilos del Navbar
+import React from "react";
+import { Link } from 'react-router-dom';
+import "./navbar.css";
 
-// Componente Navbar
-const Navbar = () => (
-  <nav className="navbar">
-    <h1><Link to="/ " className="navbarTittle">CALI TRAVEL GUIDE</Link></h1>
+const Navbar = () => {
+  // Función para cerrar el menú
+  const closeMenu = () => {
+    const menuToggle = document.getElementById('menuToggle');
+    if (menuToggle) menuToggle.checked = false;
+  };
 
-    {/* Checkbox para el menú hamburguesa */}
-    <input type="checkbox" id="menuToggle" />
-    <label htmlFor="menuToggle" id="menuIcon">☰</label>
+  return (
+    <nav className="navbar">
+      <h1><Link to="/" className="navbarTittle">CALI TRAVEL GUIDE</Link></h1>
 
-    {/* Menú */}
-    <ul className="navbarMenu" id="menuIconAcction">
-      <li><Link to="/" className="navbarLink">INICIO</Link></li> {/* Redirige al Home */}
-      <li><Link to="/lugares" className="navbarLink">LUGARES</Link></li>
-      <li><Link to="/contactenos" className="navbarLink">CONTACTENOS</Link></li>
-    </ul>
-  </nav>
-);
+      <input type="checkbox" id="menuToggle" />
+      <label htmlFor="menuToggle" id="menuIcon">☰</label>
 
-// Exportar el componente Navbar
+      <ul className="navbarMenu" id="menuIconAcction">
+        <li><Link to="/" className="navbarLink" onClick={closeMenu}>INICIO</Link></li>
+        <li><Link to="/lugares" className="navbarLink" onClick={closeMenu}>LUGARES</Link></li>
+        <li><Link to="/contactenos" className="navbarLink" onClick={closeMenu}>CONTACTENOS</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
 export default Navbar;
