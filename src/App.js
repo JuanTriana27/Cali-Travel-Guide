ad8f773c35755ca8c716b78e9459cc1442c8084c
Juan Esteban <trianajuan28@gmail.com>
2025-03-08 11:51:35 -0500

    Segundo ajuste Cards

--- src/App.js
+++ src/App.js
@@ -3,19 +3,35 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/navbar.jsx';
 import Home from './pages/Home/home.jsx';
 import Footer from './components/Footer/footer.jsx';
-import Historia from './pages/Historia/historia.jsx'; 
-import Fotografias from './pages/Fotografias/fotografias.jsx'; 
+import Historia from './pages/Historia/historia.jsx';
+import Fotografias from './pages/Fotografias/fotografias.jsx';
+
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App" style={{ 
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        position: 'relative'
+      }}>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/historia" element={<Historia />} />
-          <Route path="/fotografias" element={<Fotografias />} />
-        </Routes>
-        <Footer /> {/* Agregamos el componente del footer */}
+        
+        {/* Contenido principal */}
+        <main style={{
+          flex: 1,
+          
+          width: '100%',
+          overflowX: 'hidden'
+        }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/historia" element={<Historia />} />
+            <Route path="/fotografias" element={<Fotografias />} />
+          </Routes>
+        </main>
+        
+        <Footer />
       </div>
     </Router>
   );
